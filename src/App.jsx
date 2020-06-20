afdea46e85c6961f71c5cae03ffaaf098f52a4d2
gringrape <gringrape200@gmail.com>
2020-06-20 16:19:17 +0900

    markup

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <div className="element"></div>
+        <p>count</p>
+        <button className="button">play</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
